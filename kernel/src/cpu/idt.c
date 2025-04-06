b97e467e647791764e30d1e0db8ecb2cd4022382
Finorest <dwd2010.4@gmail.com>
2025-04-06 12:30:19 +0200

    Update: (date) 2025-04-06 10:30:19 (info) IDT remade - Fix1

--- kernel/src/cpu/idt.c
+++ kernel/src/cpu/idt.c
@@ -5,7 +5,7 @@
 IDTEntry g_IDT[256];
 IDTDescriptor g_IDTDescriptor = { sizeof(g_IDT) - 1, g_IDT };
 
-void __attribute__((cdecl)) IDTLoad(IDTDescriptor* idtDescriptor);
+extern void __attribute__((cdecl)) IDTLoad(IDTDescriptor* idtDescriptor);
 
 void IDT_SetGate(int interrupt, void* base, u16_t segmentDescriptor, u8_t flags) {
   g_IDT[interrupt].BaseLow = ((u16_t)base) & 0xFFFF;
@@ -13,7 +13,7 @@ void IDT_SetGate(int interrupt, void* base, u16_t segmentDescriptor, u8_t flags)
   g_IDT[interrupt].Reserved = 0;
   g_IDT[interrupt].Flags = flags;
   g_IDT[interrupt].BaseMiddle = ((u16_t)base >> 16) & 0xFFFF;
-  g_IDT[interrupt].BaseHigh = ((uint32_t)base >> 32) & 0xFFFFFFFF;
+  g_IDT[interrupt].BaseHigh = ((u32_t)base >> 32) & 0xFFFFFFFF;
   g_IDT[interrupt].Reserved1 = 0;
 }
 
