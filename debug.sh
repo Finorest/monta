079a7ca4d036bfecb31a24e4382fb227db1b0fc8
Finorest <dwd2010.4@gmail.com>
2025-04-12 14:26:18 +0200

    Update: (date) 2025-04-12 12:26:18 (info) ISR made, tests. Fix 3.2.1

--- debug.sh
+++ debug.sh
@@ -3,7 +3,7 @@
 if test -f os.img
 then
     qemu-system-x86_64 -drive file=os.img,format=raw -s -S --no-reboot --no-shutdown &
-    gdb -ex "target remote localhost:1234" -ex "symbol-file kernel/kernel.elf"
+    gdb -ex "file kernel/kernel.elf" -ex "target remote localhost:1234" -ex "symbol-file kernel/kernel.elf"
 else
     sh build.sh
 
